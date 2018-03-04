22e23fd5afe8e3cb8e4fe1d29483018d60da9752
Lin Yun Wen <l40303k@gmail.com>
2018-03-04 20:37:10 +0800

    Feature: reduce the struct size
    To reduce the rate of cache miss, modify the __PHONE_BOOK_ENTRY.
    Keep findName and append the same in ones of origin.

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -6,9 +6,7 @@
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
 // #define OPT 1
-typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
-    char firstName[16];
+typedef struct __INFOMATION {
     char email[16];
     char phone[10];
     char cell[10];
@@ -17,6 +15,12 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
+} info_entry;
+
+typedef struct __PHONE_BOOK_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+    char firstName[16];
+    info_entry *infomation;
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
 
